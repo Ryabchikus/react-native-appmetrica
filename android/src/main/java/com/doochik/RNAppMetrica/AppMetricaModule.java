42dcc2691c2756240169e6dce12b3979564998a6
Evgeniy Kozhich <wanderbon95@gmail.com>
2021-04-26 13:28:48 +0300

    Update AppMetricaModule.java

--- android/src/main/java/com/doochik/RNAppMetrica/AppMetricaModule.java
+++ android/src/main/java/com/doochik/RNAppMetrica/AppMetricaModule.java
@@ -198,6 +198,10 @@ public class AppMetricaModule extends ReactContextBaseJavaModule {
 
             if(userProfile.getUserProfileUpdates().size() > 0) {
                 YandexMetrica.reportUserProfile(userProfile);
+
+                promise.resolve(userProfileParam);
+            } else {
+                promise.reject("-102", "Valid keys not found");
             }
         }
     }
